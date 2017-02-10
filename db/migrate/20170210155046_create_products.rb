2421b7547065f85705f7af7c2f6a412e6b96f121
Loren Glenn <lorencglenn@gmail.com>
2017-02-10 07:54:23 -0800

    generate model

---
+++ db/migrate/20170210155046_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+
+      t.column :name, :string
+      t.column :cost, :integer
+      t.column :country, :string
+      t.column :image, :string
+
+      t.timestamps
+    end
+  end
+end
