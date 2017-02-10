dc639d3d6a1731474645e8db96bd3e898b08c4da
Loren Glenn <lorencglenn@gmail.com>
2017-02-10 08:03:54 -0800

    create views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,35 @@
 class ProductsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country, :image)
+  end
 end
