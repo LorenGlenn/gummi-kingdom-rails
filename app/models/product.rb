d2da5605aec6baf581b00a307a5616d5181befa3
Loren Glenn <lorencglenn@gmail.com>
2017-02-10 11:35:23 -0800

    add comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,6 @@ class Product < ApplicationRecord
   validates :cost, :presence => true
   validates :country, :presence => true
   validates :image, :presence => true
+  #Not actually a good test, user could just add false file extension
   validates_format_of :image, :with => %r{\.(png|jpg|jpeg)$}i, :multiline => true, :message => "Invalid File Format, must be of type .png .jpg  or .jpeg"
 end
